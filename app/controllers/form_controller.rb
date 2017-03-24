6663ed41d7484d28697a60a1406cf74d53e4b2ad
Yusuke Oshiro <yoshiro@yoshiro-ltm.internal.salesforce.com>
2017-03-24 16:56:03 +0900

    added salesforce wrapper

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -27,25 +27,35 @@ class FormController < ApplicationController
 
 	def submit
 
-		# todo upsert to salesforce
-		
+		# upsert to salesforce
+			s = SforceWrapper.new(ENV["SFDC_EMAIL"],ENV["SFDC_PASSWORD"])
+			
+			payload = {
+				"LastName" => params["last_name"],
+				"FirstName" => params["first_name"],
+				"email" => params["email"],
+				"accountid" => "00146000004aS1E",
+				"recordtypeid" => "01246000000rEL0"
+			}
+			s.insert_record( "Contact", payload )
 
-		
-		if params["email"]
-			visitor_id = ""
-			cookies.each do |cookie|
-				visitor_id = cookie[1] if cookie[0] == ENV["PARDOT_VISITOR_ID_KEY"]				
-			end
 
-			if visitor_id 
-				new_item = QueuedItem.new
-				new_item.email = params["email"]
-				new_item.visitor_id = visitor_id
-				new_item.save
-				flash[:submitted] = true
+		# insert to the queue
+			if params["email"]
+				visitor_id = ""
+				cookies.each do |cookie|
+					visitor_id = cookie[1] if cookie[0] == ENV["PARDOT_VISITOR_ID_KEY"]				
+				end
+
+				if visitor_id 
+					new_item = QueuedItem.new
+					new_item.email = params["email"]
+					new_item.visitor_id = visitor_id
+					new_item.save
+					flash[:submitted] = true
+				end
 			end
-		end
-		
+			
 		redirect_to show_form_path
 	end
 
