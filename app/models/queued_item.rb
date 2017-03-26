f7e3156a89f96e7fd24f9e4123c9edfb36b7732b
Yusuke Oshiro <yusuke@oshiro1.com>
2017-03-26 16:37:21 +0900

    debugged scheduler

--- app/models/queued_item.rb
+++ app/models/queued_item.rb
@@ -1,5 +1,14 @@
 class QueuedItem < ApplicationRecord
 	scope :remaining, -> {where(finished: false)}
+	after_initialize :set_default
+
+	def set_default
+		self.finished                = false if self.finished.nil?
+		self.assignment_complete     = false if self.assignment_complete.nil?
+		self.form_handler_complete   = false if self.form_handler_complete.nil?
+		self.contact_update_complete = false if self.contact_update_complete.nil?
+		
+	end
 
 	def check_complete
 		return self.assignment_complete && self.form_handler_complete && self.contact_update_complete			
