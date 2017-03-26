2255e3048c3fb5094db1634f90ae646c5a49d7d0
Yusuke Oshiro <yusuke@oshiro1.com>
2017-03-26 16:25:40 +0900

    debugged model

--- app/models/queued_item.rb
+++ app/models/queued_item.rb
@@ -1,13 +1,5 @@
 class QueuedItem < ApplicationRecord
-	after_initialize :set_default
 	scope :remaining, -> {where(finished: false)}
-
-	def set_default
-		self.finished = false
-		self.assignment_complete = false
-		self.form_handler_complete = false
-		self.contact_update_complete = false
-	end
 
 	def check_complete
 		return self.assignment_complete && self.form_handler_complete && self.contact_update_complete			
